fac0a5a9cc1c6c9e972f5de341d689712e85a46c
martyn <martyn@iconbuild.com>
2023-05-31 14:34:08 -0500

    el3004 kinda working...

--- src/manager.cc
+++ src/manager.cc
@@ -33,6 +33,8 @@
 #include "fastcat/jsd/egd_offline.h"
 #include "fastcat/jsd/el2124.h"
 #include "fastcat/jsd/el2124_offline.h"
+#include "fastcat/jsd/el3004.h"
+#include "fastcat/jsd/el3004_offline.h"
 #include "fastcat/jsd/el3104.h"
 #include "fastcat/jsd/el3104_offline.h"
 #include "fastcat/jsd/el3162.h"
@@ -435,6 +437,9 @@ bool fastcat::Manager::ConfigJSDBusFromYaml(YAML::Node node)
 
     } else if (0 == device_class.compare("El4102")) {
       device = std::make_shared<El4102>();
+
+    } else if (0 == device_class.compare("El3004")) {
+      device = std::make_shared<El3004>();
 
     } else if (0 == device_class.compare("El3162")) {
       device = std::make_shared<El3162>();
